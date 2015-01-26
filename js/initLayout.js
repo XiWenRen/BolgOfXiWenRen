57f3111776dd36641adf4311a683b47ac14c50fc
qiaoranmengmei <qiaoranmengmei@qiaoran>
2015-01-26 14:37:47 +0800

    评论模块

--- js/initLayout.js
+++ js/initLayout.js
@@ -6,6 +6,16 @@ var initArticalMargin = function() {
 	var menuItem = document.getElementById("menu");
 	var titleItem = document.getElementById("titles");
 	
+	for(var i=1;i<4;i++){ 
+	var str1 = "commends_communication_"+i;
+	var str2 = "commends_communication_color_"+i;
+	var comments_color = document.getElementById(str1);
+	var comments_color_after = document.getElementById(str2);
+	var bgColor = getRandomColor();
+	comments_color.style.backgroundColor = bgColor;
+	comments_color_after.style.borderRightColor = bgColor;
+	}
+	
 	menuItem.onmouseup = function() {
 		toggleMenuItem();
 //		console.log(menuItem.style.opacity);
@@ -16,7 +26,6 @@ var initArticalMargin = function() {
 //		}
 		if(hasClass(titleItem,"title_narrow")){
 			toggleTitlesItem();
-		
 		}
 	}
 	titleItem.onmouseup = function() {
@@ -26,6 +35,15 @@ var initArticalMargin = function() {
 		}
 	}
 }
+
+var getRandomColor = function(){    
+
+  return  '#' +    
+    (function(color){    
+    return (color +=  '0123456789abcdef'[Math.floor(Math.random()*16)])    
+      && (color.length == 6) ?  color : arguments.callee(color);    
+  })('');    
+} 
 
 var toggleMenuItem = function(){
 	var menuItem = document.getElementById("menu");
