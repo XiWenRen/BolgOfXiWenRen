8579b88b78aa2c303faeb82847336e5b457b5dd3
qiaoranmengmei <qiaoranmengmei@qiaoran>
2015-01-28 21:00:17 +0800

    第二第三栏显示优化

--- js/initLayout.js
+++ js/initLayout.js
@@ -1,85 +1,139 @@
-window.onload = function() {
-	initArticalMargin();
-}
+//window.onload = function() {
+//	InitArticalMargin();
+//}
 
-var initArticalMargin = function() {
-	var menuItem = document.getElementById("menu");
-	var titleItem = document.getElementById("titles");
-	
-	for(var i=1;i<4;i++){ 
-	var str1 = "commends_communication_"+i;
-	var str2 = "commends_communication_color_"+i;
-	var comments_color = document.getElementById(str1);
-	var comments_color_after = document.getElementById(str2);
-	var bgColor = getRandomColor();
-	comments_color.style.backgroundColor = bgColor;
-	comments_color_after.style.borderRightColor = bgColor;
-	}
-	
-	menuItem.onmouseup = function() {
-		toggleMenuItem();
-//		console.log(menuItem.style.opacity);
-//		if(menuItem.style.opacity==100){
-//			menuItem.style.opacity=0;
-//		}else{
-//			menuItem.style.opacity=100;
-//		}
-		if(hasClass(titleItem,"title_narrow")){
-			toggleTitlesItem();
+var InitArticalMargin = function() {
+	var MenuItem = document.getElementById("menu");
+	var TitleItem = document.getElementById("titles");
+	var MenuClick = document.getElementById("click_menu");
+	MenuClick.onmouseup = function() {
+		ToggleMenuItem();
+		if (HasClass(TitleItem, "title_narrow")) {
+			ToggleTitlesItem();
 		}
 	}
-	titleItem.onmouseup = function() {
-		toggleTitlesItem();
-		if(!hasClass(menuItem,"menu_narrow")){
-			toggleMenuItem();
+	TitleItem.onmouseup = function() {
+		ToggleTitlesItem();
+		if (!HasClass(MenuItem, "menu_narrow")) {
+			ToggleMenuItem();
 		}
 	}
 }
 
-var getRandomColor = function(){    
-
-  return  '#' +    
-    (function(color){    
-    return (color +=  '0123456789abcdef'[Math.floor(Math.random()*16)])    
-      && (color.length == 6) ?  color : arguments.callee(color);    
-  })('');    
-} 
-
-var toggleMenuItem = function(){
-	var menuItem = document.getElementById("menu");
-	var articalItem = document.getElementById("article");
-	toggleClass(menuItem, "menu_narrow");
-	toggleClass(articalItem, "article_wide");
+var mouse_menu_fun = function() {
+	var MenuMouseon = document.getElementById("click_menu");
+	MenuMouseon.style.display = "";
 }
 
-var toggleTitlesItem = function(){
-	var titleItem = document.getElementById("titles");
-	var content = document.getElementById("content");
-	toggleClass(titles, "title_narrow");
-	toggleClass(content, "content_wide");
+var mousemove_menu = function() {
+	var MenuMousemove = document.getElementById("click_menu");
+	MenuMousemove.style.display = "none";
 }
 
-function hasClass(obj, cls) {
+var mousefun = function() {
+	var TitleNum = event.target.id;
+	var IdNum = TitleNum.charAt(11);
+	for (var j = 1; j < 7; j++) {
+		if (j != IdNum) {
+			var ContentNum = "content_id_" + j;
+			var Content = document.getElementById(ContentNum);
+			Content.style.display = "none";
+		} else {
+			var ContentNum = "content_id_" + IdNum;
+			var Content = document.getElementById(ContentNum);
+			Content.style.display = "";
+			for (var i = 1; i < 4; i++) {
+				var CommentsColorId = "commends_communication_" + IdNum + "_" + i;
+				var CommentsColorAfterId = "commends_communication_color_" + IdNum + "_" + i;
+				var CommentsColor = document.getElementById(CommentsColorId);
+				var CommentsColorAfter = document.getElementById(CommentsColorAfterId);
+				var BgColor = GetRandomColor();
+				CommentsColor.style.backgroundColor = BgColor;
+				CommentsColorAfter.style.borderRightColor = BgColor;
+			}
+		}
+	}
+}
+
+var GetRandomColor = function() {
+	var arr = ["#fde8fd","#f1defd","#fddef0","#fcd5d5","#fce8d5","#fcfad5","#f2fcd5","#dffcd5","#fcd6e7","#effcd6","#d6fcf3","#d5ebfc","#fec1fd"];
+	var index = Math.floor((Math.random() * arr.length));
+	return arr[index];
+}
+
+var ToggleMenuItem = function() {
+	var MenuItem = document.getElementById("menu");
+	var ArticalItem = document.getElementById("article");
+	ToggleClass(MenuItem, "menu_narrow");
+	ToggleClass(ArticalItem, "article_wide");
+}
+
+var ToggleTitlesItem = function() {
+	var TitleItem = document.getElementById("titles");
+	var Content = document.getElementById("content");
+	ToggleClass(TitleItem, "title_narrow");
+	ToggleClass(Content, "content_wide");
+}
+
+var Rotate = document.getElementById("rotate_title"),n=0,rotINT;
+var RotateTitle = function() {
+	var Rotate = document.getElementById("rotate_title");	
+	clearInterval(rotINT)
+	rotINT=setInterval("startRotate()",100)
+}
+
+var RotateTitleEnd = function() {
+	var Rotate = document.getElementById("rotate_title");	
+	clearInterval(rotINT)
+	rotINT=setInterval("endRotate()",1)
+}
+
+function HasClass(obj, cls) {
 	return obj.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
 }
 
-function addClass(obj, cls) {
-	if (!this.hasClass(obj, cls)) {
+function AddClass(obj, cls) {
+	if (!this.HasClass(obj, cls)) {
 		obj.className += " " + cls;
 	}
 }
 
-function removeClass(obj, cls) {
-	if (hasClass(obj, cls)) {
+function RemoveClass(obj, cls) {
+	if (HasClass(obj, cls)) {
 		var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
 		obj.className = obj.className.replace(reg, ' ');
 	}
 }
 
-function toggleClass(obj, cls) {
-	if (hasClass(obj, cls)) {
-		removeClass(obj, cls);
+function ToggleClass(obj, cls) {
+	if (HasClass(obj, cls)) {
+		RemoveClass(obj, cls);
 	} else {
-		addClass(obj, cls);
+		AddClass(obj, cls);
+	}
+}
+
+var startRotate = function(){
+	n = n + 1
+	Rotate.style.transform = "rotate(" + n + "deg)"
+	Rotate.style.webkitTransform = "rotate(" + n + "deg)"
+	Rotate.style.OTransform = "rotate(" + n + "deg)"
+	Rotate.style.MozTransform = "rotate(" + n + "deg)"
+	if (n == 180 || n == 360) {
+		clearInterval(rotINT)
+		if (n == 360) {
+			n = 0
+		}
+	}
+}
+
+var endRotate = function() {
+	n = n + 1
+	Rotate.style.transform = "rotate(" + n + "deg)"
+	Rotate.style.webkitTransform = "rotate(" + n + "deg)"
+	Rotate.style.OTransform = "rotate(" + n + "deg)"
+	Rotate.style.MozTransform = "rotate(" + n + "deg)"
+	if (n == 90) {
+			n = 0;
 	}
 }
